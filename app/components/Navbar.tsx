34121be41d765074bfeee5cad29aa96c960a1103
Faisal Misbah <132295428+FaisalMisbah23@users.noreply.github.com>
2024-02-04 16:27:25 +0500

    Frontend Done

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,25 +3,29 @@ import { Themetoggle } from "./Themetoggle";
 import { Button } from "@/components/ui/button";
 import {RegisterLink, LoginLink,LogoutLink} from "@kinde-oss/kinde-auth-nextjs/components";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { UserNav } from "./UserNav";
 
 
 const Navbar =async () => {
-  const { isAuthenticated } = getKindeServerSession();
+  const { isAuthenticated,getUser } = getKindeServerSession();
+  const user= await getUser();
   return (
     <nav className="border-b bg-background h-[10vh] flex items-center">
       <div className="container flex items-center justify-between">
         <Link href="/">
           <h1 className="font-bold text-3xl">
-            Take <span className="text-primary">Notes</span>
+            My <span className="text-primary">Notes</span>
           </h1>
         </Link>
         <div className="flex items-center gap-x-5">
           <Themetoggle />
           <div className="flex items-center gap-x-5">
           {(await isAuthenticated()) ? (
-            <LogoutLink>
-            <Button>Logout</Button>
-          </LogoutLink>
+             <UserNav
+             email={user?.email as string}
+             image={user?.picture as string}
+             name={user?.given_name as string}
+           />
           ) : (
             <div className="flex items-center gap-x-5">
               <LoginLink>
